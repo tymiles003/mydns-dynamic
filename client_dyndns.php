a33a681672a981486f81d423c58ecfff8e3a5f4f
corpix <me@corpix.ru>
2012-03-24 15:10:30 +0400

    Refactoring

--- client_dyndns.php
+++ client_dyndns.php
@@ -1,38 +1,39 @@
 <?php
 
 $host = 'https://somedomain.ru/dyndns.php';
-$username = 'user';
-$password = 'password';
 $secret = 'keyboard cat';
 
+// ------------------------------------------ //
+
 $params = array(
-	array(
-		'secret' => $secret,
-		'domain' => 'somedomain.ru',
-		'subdomain' => 'me'
-	)
+  array(
+    'secret' => $secret,
+  )
 );
 
 foreach($params as $param) {
-	$c = curl_init();
-	               
-	curl_setopt_array($c, array(
-	  CURLOPT_URL => $host.'?'.http_build_query($param),
-	  CURLOPT_HEADER => false,
-	  CURLOPT_USERPWD => $username . ":" . $password,
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_SSL_VERIFYPEER => false,
-	  
-	  CURLOPT_VERBOSE => false,
-	));
-	
-	$res = curl_exec($c);
-	curl_close($c);
-	$res = json_decode($res);
-	if($res) {
-		if($res->changed) {
-			if(exec('which notify-send')) exec("DISPLAY=:0 notify-send -t 10 -u low 'Dynamic DNS' 'IP обновлён\\n{$res->current}' -i /usr/share/icons/Humanity/actions/48/rotate.svg");
-		}
-	}
+  $c = curl_init();
+
+  curl_setopt_array($c, array(
+    CURLOPT_URL => $host
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+
+    CURLOPT_POST => count($params),
+    CURLOPT_POSTFIELDS => http_build_query($param),
+
+    CURLOPT_VERBOSE => false,
+  ));
+
+  $res = curl_exec($c);
+  curl_close($c);
+  $res = json_decode($res);
+
+  if($res) {
+    if($res->changed) {
+      if(exec('which notify-send')) exec("DISPLAY=:0 notify-send -t 10 -u low 'Dynamic DNS' 'IP обновлён\\n{$res->current}' -i /usr/share/icons/Humanity/actions/48/rotate.svg");
+    }
+  }
 }
 
